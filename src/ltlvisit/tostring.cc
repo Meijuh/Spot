c9b34d684ac6dd556d3ff38ade412b5d7a19138e
Alexandre Duret-Lutz <adl@lrde.epita.fr>
2011-06-16 11:55:05 +0200

    * src/ltlvisit/tostring.cc: Output <-> and -> instead of <=> and =>.

--- src/ltlvisit/tostring.cc
+++ src/ltlvisit/tostring.cc
@@ -1,7 +1,7 @@
-// Copyright (C) 2008, 2010, 2011 Laboratoire de Recherche et
-// Développement de l'Epita (LRDE).
+// Copyright (C) 2008, 2010, 2012 Laboratoire de Recherche et DÃ©veloppement
+// de l'Epita (LRDE).
 // Copyright (C) 2003, 2004 Laboratoire d'Informatique de Paris 6 (LIP6),
-// département Systèmes Répartis Coopératifs (SRC), Université Pierre
+// dÃ©partement SystÃ¨mes RÃ©partis CoopÃ©ratifs (SRC), UniversitÃ© Pierre
 // et Marie Curie.
 //
 // This file is part of Spot, a model checking library.
@@ -142,10 +142,10 @@ namespace spot
 	      os_ << " ^ ";
 	      break;
 	    case binop::Implies:
-	      os_ << " => ";
+	      os_ << " -> ";
 	      break;
 	    case binop::Equiv:
-	      os_ << " <=> ";
+	      os_ << " <-> ";
 	      break;
 	    case binop::U:
 	      os_ << " U ";
