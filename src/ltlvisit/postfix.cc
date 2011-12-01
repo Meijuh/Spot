ec9a3f96cb0f1b47bd8bf284df4d065c00c607a9
Alexandre Duret-Lutz <adl@lrde.epita.fr>
2011-12-01 21:18:27 +0100

    * src/ltlvisit/postfix.cc: Fix recursion on bunop formulae.

--- src/ltlvisit/postfix.cc
+++ src/ltlvisit/postfix.cc
@@ -1,7 +1,7 @@
-// Copyright (C) 2009, 2010  Laboratoire de Recherche et Développement
-// de l'Epita (LRDE).
+// Copyright (C) 2009, 2010, 2011 Laboratoire de Recherche et
+// DÃ©veloppement de l'Epita (LRDE).
 // Copyright (C) 2003  Laboratoire d'Informatique de Paris 6 (LIP6),
-// département Systèmes Répartis Coopératifs (SRC), Université Pierre
+// dÃ©partement SystÃ¨mes RÃ©partis CoopÃ©ratifs (SRC), UniversitÃ© Pierre
 // et Marie Curie.
 //
 // This file is part of Spot, a model checking library.
@@ -78,6 +78,7 @@ namespace spot
     void
     postfix_visitor::visit(bunop* so)
     {
+      so->child()->accept(*this);
       doit(so);
     }
 
