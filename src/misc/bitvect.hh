Meijuh/Spot

commit 522373984cd5100890b8fb641861fd1139b9e028

bitvect: fix some issues observed on 32bit architectures.
* src/misc/bitvect.cc, src/misc/bitvect.hh: Do
not assume the two bitvect with the same size have
the same number of allocated blocks.  Fix an
assertion in extra_range().

src/misc/bitvect.hh
@@ -25,7 +25,9 @@
 #  include <cstdlib>
 #  include <cassert>
 #  include <iosfwd>
-# include <iostream>
+#  include <iostream>
+#  include <algorithm>
+
 namespace spot
 {
   /// \addtogroup misc_tools
@@ -125,6 +127,13 @@ namespace spot
     }
 
   public:
+
+    size_t used_blocks() const
+    {
+      const size_t bpb = 8 * sizeof(block_t);
+      return (size_ - 1) / bpb + 1;
+    }
+
     /// Append one bit.
     void push_back(bool val)
     {
@@ -256,24 +265,27 @@ namespace spot
 
     bitvect& operator|=(const bitvect& other)
     {
-      assert(other.block_count_ <= block_count_);
-      for (size_t i = 0; i < other.block_count_; ++i)
+      assert(other.size_ <= size_);
+      unsigned m = std::min(other.block_count_, block_count_);
+      for (size_t i = 0; i < m; ++i)
 	storage_[i] |= other.storage_[i];
       return *this;
     }
 
     bitvect& operator&=(const bitvect& other)
     {
-      assert(other.block_count_ <= block_count_);
-      for (size_t i = 0; i < other.block_count_; ++i)
+      assert(other.size_ <= size_);
+      unsigned m = std::min(other.block_count_, block_count_);
+      for (size_t i = 0; i < m; ++i)
 	storage_[i] &= other.storage_[i];
       return *this;
     }
 
     bitvect& operator^=(const bitvect& other)
     {
-      assert(other.block_count_ <= block_count_);
-      for (size_t i = 0; i < other.block_count_; ++i)
+      assert(other.size_ <= size_);
+      unsigned m = std::min(other.block_count_, block_count_);
+      for (size_t i = 0; i < m; ++i)
 	storage_[i] ^= other.storage_[i];
       return *this;
     }
@@ -288,10 +300,13 @@ namespace spot
 
     bool operator==(const bitvect& other) const
     {
-      if (other.block_count_ != block_count_)
+      if (other.size_ != size_)
 	return false;
+      if (size_ == 0)
+	return true;
       size_t i;
-      for (i = 0; i < other.block_count_ - 1; ++i)
+      size_t m = other.used_blocks();
+      for (i = 0; i < m - 1; ++i)
 	if (storage_[i] != other.storage_[i])
 	  return false;
       // The last block might not be fully used, compare only the
@@ -308,10 +323,13 @@ namespace spot
 
     bool operator<(const bitvect& other) const
     {
-      if (block_count_ != other.block_count_)
-	return block_count_ < other.block_count_;
+      if (size_ != other.size_)
+	return size_ < other.size_;
+      if (size_ == 0)
+	return false;
       size_t i;
-      for (i = 0; i < other.block_count_ - 1; ++i)
+      size_t m = other.used_blocks();
+      for (i = 0; i < m - 1; ++i)
 	if (storage_[i] > other.storage_[i])
 	  return false;
       // The last block might not be fully used, compare only the
@@ -374,7 +392,7 @@ namespace spot
 	      ++indexb;
 	      res->push_back(storage_[indexb], bpb);
 	      count -= bpb;
-	      assert(indexb != indexe || bpb == 0);
+	      assert(indexb != indexe || count == 0);
 	    }
 	  if (count > 0)
 	    {

src/misc/bitvect.cc
@@ -120,7 +120,10 @@ namespace spot
 
     block_t res = fnv<sizeof(block_t)>::init();
     size_t i;
-    for (i = 0; i < block_count_ - 1; ++i)
+    size_t m = used_blocks();
+    if (m == 0)
+      return res;
+    for (i = 0; i < m - 1; ++i)
       {
 	res ^= storage_[i];
 	res *= fnv<sizeof(block_t)>::prime();

commit 4f31a9bbed78cd3f019352202ddea4b2559436ca

bitvect: fix block_count() on 0-sized bit-vectors
The bug was introduced by 522373984cd5100890b8fb641861fd1139b9e028.

* src/misc/bitvect.hh (block_count): Here.
* src/tgbatest/dstar.test: Add a test case.

src/misc/bitvect.hh
@@ -131,7 +131,7 @@ namespace spot
     size_t used_blocks() const
     {
       const size_t bpb = 8 * sizeof(block_t);
-      return (size_ - 1) / bpb + 1;
+      return (size_ + bpb - 1) / bpb;
     }
 
     /// Append one bit.

src/tgbatest/dstar.test
@@ -1,6 +1,6 @@
 #!/bin/sh
 # -*- coding: utf-8 -*-
-# Copyright (C) 2013 Laboratoire de Recherche et
+# Copyright (C) 2013, 2014 Laboratoire de Recherche et
 # Développement de l'Epita (LRDE).
 #
 # This file is part of Spot, a model checking library.
@@ -237,3 +237,33 @@ EOF
 diff expected stdout
 
 test "`../../bin/dstar2tgba -D dra.dstar --stats '%s %t %p %d'`" = "3 12 1 1"
+
+# This has caused a crash at some point when dealing with 0-sized
+# bitsets to represent acceptance sets.
+cat >aut.dsa <<EOF
+DSA v2 explicit
+Comment: "Streett{Safra[NBA=1]}"
+States: 1
+Acceptance-Pairs: 0
+Start: 0
+AP: 0
+---
+State: 0
+Acc-Sig:
+0
+EOF
+run 0 ../ltl2tgba -XDD aut.dsa | tee stdout
+
+cat >expected<<EOF
+digraph G {
+  0 [label="", style=invis, height=0]
+  0 -> 1
+  1 [label="0"]
+  1 -> 1 [label="1\n"]
+  1 -> 2 [label="1\n"]
+  2 [label="1"]
+  2 -> 2 [label="1\n"]
+}
+EOF
+
+diff expected stdout