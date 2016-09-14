dfd500a559a5607285055d6930d9cc7cb80f82a2
Alexandre GBAGUIDI AISSE <gbagui_a@epita.fr>
2016-09-14 17:19:23 +0200

    tests: Improve tests related to SAT-minimization
    * tests/core/satmin.test: Delete check for SPOT_SATSOLVER env
    variable and add state numbers verification.
    * tests/core/satmin2.test: Delete check for SPOT_SATSOLVER
    env variable.
    * tests/python/satmin.py: Delete check for SPOT_SATSOLVER
    env variable.

--- tests/python/satmin.py
+++ tests/python/satmin.py
@@ -17,13 +17,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import shutil
-import sys
 import spot
 
-# Ignore the test if glucose is not installed.
-if shutil.which("glucose") == None:
-    sys.exit(77)
 
 aut = spot.translate('GFa & GFb', 'BA')
 assert aut.num_sets() == 1
