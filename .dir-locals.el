5f564c2b450294b95755f7c10ef51f1f3f1951ff
Alexandre Duret-Lutz <adl@lrde.epita.fr>
2017-03-28 21:24:39 +0200

    emacs config: Make C-c C-c execute the current test through ./run
    * .dir-locals.el: Redefine C-c C-c in sh-mode and python-mode for
    the tests/ subdirectory.c
    * HACKING: Mention it.

--- .dir-locals.el
+++ .dir-locals.el
@@ -6,4 +6,14 @@
  (c++-mode . ((c-default-style . "gnu")
 	      (indent-tab-mode . t)
 	      (tab-width . 8)
-	      (fill-column . 70))))
+	      (fill-column . 70)))
+ ("tests" . ((nil . ((eval . (defun spot/execute-test ()
+                               "execute the current file through ../run"
+                               (interactive)
+                               (shell-command (concat "cd ..; ./run "
+                                                      (buffer-file-name)
+                                                      " &"))))))
+             (sh-mode . ((eval . (local-set-key
+                                  (kbd "C-c C-c") #'spot/execute-test))))
+             (python-mode . ((eval . (local-set-key (kbd "C-c C-c")
+                                                    #'spot/execute-test)))))))
