Meijuh/Spot

commit abe2c08b789b59b1fe2ab56d7534bb964aaca9a4

acc: make &= and |= symmetrical
Operator &= used to always move Fin to the front, it does not anymore.
The only thing it does now is to merge Inf(x)&Inf(y) as Inf({x,y}).
Operator |= is now symmetrical and merges Fin()s.

Fixes #253.

* spot/twa/acc.cc, spot/twa/acc.hh: Simplify &= and make |= symmetrical.
* spot/twaalgos/cleanacc.cc: Fix conjunction order.
* tests/core/acc.test, tests/core/acc2.test, tests/core/parseaut.test,
tests/core/readsave.test, tests/core/satmin2.test,
tests/core/sccdot.test, tests/python/acc_cond.ipynb,
tests/python/accparse.ipynb, tests/python/automata.ipynb,
tests/python/product.ipynb, tests/python/randaut.ipynb: Adjust test
cases.

spot/twa/acc.hh
@@ -578,11 +578,11 @@ namespace spot
         unsigned n = 0;
         for (Iterator i = begin; i != end; ++i)
           {
-            acc_cond::acc_code pair = fin({n++});
+            unsigned f = n++;
             acc_cond::mark_t m = 0U;
             for (unsigned ni = *i; ni > 0; --ni)
               m.set(n++);
-            pair &= inf(m);
+            auto pair = inf(m) & fin({f});
             std::swap(pair, res);
             res |= std::move(pair);
           }
@@ -596,100 +596,6 @@ namespace spot
       // acceptance formula.
       static acc_code random(unsigned n, double reuse = 0.0);
 
-      acc_code& operator&=(acc_code&& r)
-      {
-        if (is_t() || r.is_f())
-          {
-            *this = std::move(r);
-            return *this;
-          }
-        if (is_f() || r.is_t())
-          return *this;
-        unsigned s = size() - 1;
-        unsigned rs = r.size() - 1;
-        // We want to group all Inf(x) operators:
-        //   Inf(a) & Inf(b) = Inf(a & b)
-        if (((*this)[s].sub.op == acc_op::Inf
-             && r[rs].sub.op == acc_op::Inf)
-            || ((*this)[s].sub.op == acc_op::InfNeg
-                && r[rs].sub.op == acc_op::InfNeg))
-          {
-            (*this)[s - 1].mark |= r[rs - 1].mark;
-            return *this;
-          }
-
-        // In the more complex scenarios, left and right may both
-        // be conjunctions, and Inf(x) might be a member of each
-        // side.  Find it if it exists.
-        // left_inf points to the left Inf mark if any.
-        // right_inf points to the right Inf mark if any.
-        acc_word* left_inf = nullptr;
-        if ((*this)[s].sub.op == acc_op::And)
-          {
-            auto start = &(*this)[s] - (*this)[s].sub.size;
-            auto pos = &(*this)[s] - 1;
-            pop_back();
-            while (pos > start)
-              {
-                if (pos->sub.op == acc_op::Inf)
-                  {
-                    left_inf = pos - 1;
-                    break;
-                  }
-                pos -= pos->sub.size + 1;
-              }
-          }
-        else if ((*this)[s].sub.op == acc_op::Inf)
-          {
-            left_inf = &(*this)[s - 1];
-          }
-
-        acc_word* right_inf = nullptr;
-        auto right_end = &r.back();
-        if (right_end->sub.op == acc_op::And)
-          {
-            auto start = &r[0];
-            auto pos = --right_end;
-            while (pos > start)
-            {
-              if (pos->sub.op == acc_op::Inf)
-                {
-                  right_inf = pos - 1;
-                  break;
-                }
-              pos -= pos->sub.size + 1;
-            }
-          }
-        else if (right_end->sub.op == acc_op::Inf)
-          {
-            right_inf = right_end - 1;
-          }
-
-        if (left_inf && right_inf)
-          {
-            left_inf->mark |= right_inf->mark;
-            insert(this->end(), &r[0], right_inf);
-            insert(this->end(), right_inf + 2, right_end + 1);
-          }
-        else if (right_inf)
-          {
-            // Always insert Inf() at the very first entry.
-            insert(this->begin(), right_inf, right_inf + 2);
-            insert(this->end(), &r[0], right_inf);
-            insert(this->end(), right_inf + 2, right_end + 1);
-          }
-        else
-          {
-            insert(this->end(), &r[0], right_end + 1);
-          }
-
-        acc_word w;
-        w.sub.op = acc_op::And;
-        w.sub.size = size();
-        emplace_back(w);
-        return *this;
-      }
-
       acc_code& operator&=(const acc_code& r)
       {
         if (is_t() || r.is_f())
@@ -701,7 +607,8 @@ namespace spot
           return *this;
         unsigned s = size() - 1;
         unsigned rs = r.size() - 1;
-        // Inf(a) & Inf(b) = Inf(a & b)
+        // We want to group all Inf(x) operators:
+        //   Inf(a) & Inf(b) = Inf(a & b)
         if (((*this)[s].sub.op == acc_op::Inf
              && r[rs].sub.op == acc_op::Inf)
             || ((*this)[s].sub.op == acc_op::InfNeg
@@ -758,29 +665,28 @@ namespace spot
             right_inf = right_end - 1;
           }
 
+        acc_cond::mark_t carry = 0U;
         if (left_inf && right_inf)
           {
-            left_inf->mark |= right_inf->mark;
-            insert(this->end(), &r[0], right_inf);
-            insert(this->end(), right_inf + 2, right_end + 1);
+            carry = left_inf->mark;
+            auto pos = left_inf - &(*this)[0];
+            erase(begin() + pos, begin() + pos + 2);
           }
-        else if (right_inf)
-          {
-            // Always insert Inf() at the very first entry.
-            insert(this->begin(), right_inf, right_inf + 2);
-            insert(this->end(), &r[0], right_inf);
-            insert(this->end(), right_inf + 2, right_end + 1);
-          }
-        else
-          {
-            insert(this->end(), &r[0], right_end + 1);
-          }
+        auto sz = size();
+        insert(end(), &r[0], right_end + 1);
+        if (carry)
+          (*this)[sz + (right_inf - &r[0])].mark |= carry;
 
         acc_word w;
         w.sub.op = acc_op::And;
         w.sub.size = size();
         emplace_back(w);
         return *this;
+      }
+
+      acc_code operator&(const acc_code& r)
+      {
+        return *this &= r;
       }
 
       acc_code operator&(acc_code&& r)
@@ -790,20 +696,13 @@ namespace spot
         return res;
       }
 
-      acc_code operator&(const acc_code& r)
-      {
-        acc_code res = *this;
-        res &= r;
-        return res;
-      }
-
-      acc_code& operator|=(acc_code&& r)
+      acc_code& operator|=(const acc_code& r)
       {
         if (is_t() || r.is_f())
           return *this;
         if (is_f() || r.is_t())
           {
-            *this = std::move(r);
+            *this = r;
             return *this;
           }
         unsigned s = size() - 1;
@@ -817,21 +716,70 @@ namespace spot
             (*this)[s - 1].mark |= r[rs - 1].mark;
             return *this;
           }
+
+        // In the more complex scenarios, left and right may both
+        // be disjunctions, and Fin(x) might be a member of each
+        // side.  Find it if it exists.
+        // left_inf points to the left Inf mark if any.
+        // right_inf points to the right Inf mark if any.
+        acc_word* left_fin = nullptr;
         if ((*this)[s].sub.op == acc_op::Or)
-          pop_back();
-        if (r.back().sub.op == acc_op::Or)
-          r.pop_back();
-        insert(this->end(), r.begin(), r.end());
+          {
+            auto start = &(*this)[s] - (*this)[s].sub.size;
+            auto pos = &(*this)[s] - 1;
+            pop_back();
+            while (pos > start)
+              {
+                if (pos->sub.op == acc_op::Fin)
+                  {
+                    left_fin = pos - 1;
+                    break;
+                  }
+                pos -= pos->sub.size + 1;
+              }
+          }
+        else if ((*this)[s].sub.op == acc_op::Fin)
+          {
+            left_fin = &(*this)[s - 1];
+          }
+
+        const acc_word* right_fin = nullptr;
+        auto right_end = &r.back();
+        if (right_end->sub.op == acc_op::Or)
+          {
+            auto start = &r[0];
+            auto pos = --right_end;
+            while (pos > start)
+            {
+              if (pos->sub.op == acc_op::Fin)
+                {
+                  right_fin = pos - 1;
+                  break;
+                }
+              pos -= pos->sub.size + 1;
+            }
+          }
+        else if (right_end->sub.op == acc_op::Fin)
+          {
+            right_fin = right_end - 1;
+          }
+
+        acc_cond::mark_t carry = 0U;
+        if (left_fin && right_fin)
+          {
+            carry = left_fin->mark;
+            auto pos = (left_fin - &(*this)[0]);
+            this->erase(begin() + pos, begin() + pos + 2);
+          }
+        auto sz = size();
+        insert(end(), &r[0], right_end + 1);
+        if (carry)
+          (*this)[sz + (right_fin - &r[0])].mark |= carry;
         acc_word w;
         w.sub.op = acc_op::Or;
         w.sub.size = size();
         emplace_back(w);
         return *this;
-      }
-
-      acc_code& operator|=(const acc_code& r)
-      {
-        return *this |= acc_code(r);
       }
 
       acc_code operator|(acc_code&& r)
@@ -841,11 +789,9 @@ namespace spot
         return res;
       }
 
-      acc_code operator|(const acc_code& r)
+      acc_code& operator|(const acc_code& r)
       {
-        acc_code res = *this;
-        res |= r;
-        return res;
+        return *this |= r;
       }
 
       acc_code& operator<<=(unsigned sets)

tests/python/acc_cond.ipynb
@@ -15,7 +15,7 @@
    "name": "python",
    "nbconvert_exporter": "python",
    "pygments_lexer": "ipython3",
-   "version": "3.5.3"
+   "version": "3.5.3+"
   },
   "name": ""
  },
@@ -489,7 +489,7 @@
        "output_type": "pyout",
        "prompt_number": 17,
        "text": [
-        "(Fin(1) & Inf(0)) | Inf(2)"
+        "(Inf(0) & Fin(1)) | Inf(2)"
        ]
       }
      ],
@@ -571,7 +571,7 @@
        "output_type": "pyout",
        "prompt_number": 20,
        "text": [
-        "(Fin(3) | Inf(1)) & (Fin(0)|Fin(2)) & Inf(4)"
+        "(Fin(3) | Inf(1)) & Inf(4) & (Fin(0)|Fin(2))"
        ]
       }
      ],
@@ -619,7 +619,7 @@
        "output_type": "pyout",
        "prompt_number": 22,
        "text": [
-        "Fin(0) & (Fin(2) | Inf(1)) & (Fin(4) | Inf(1) | Inf(3))"
+        "Fin(0) & (Inf(1) | Fin(2)) & (Inf(1) | Inf(3) | Fin(4))"
        ]
       }
      ],
@@ -720,8 +720,8 @@
        "output_type": "stream",
        "stream": "stdout",
        "text": [
-        "(Fin(0) & Inf(1)) | (Fin(2) & Inf(3))\n",
-        "(Inf(0) | Fin(1)) & (Inf(2) | Fin(3))\n"
+        "((Fin(4) & Inf(5)) | (Fin(6) & Inf(7))) & ((Fin(0) & Inf(1)) | (Fin(2) & Inf(3)))\n",
+        "((Inf(4) | Fin(5)) & (Inf(6) | Fin(7))) | ((Inf(0) | Fin(1)) & (Inf(2) | Fin(3)))\n"
        ]
       }
      ],

spot/twa/acc.cc
@@ -914,7 +914,7 @@ namespace spot
     while ((cube = isop.next()) != bddfalse)
       {
         mark_t i = 0U;
-        acc_code c;
+        acc_code f;
         while (cube != bddtrue)
           {
             // The acceptance set associated to this BDD variable
@@ -928,8 +928,7 @@ namespace spot
                 // The strange order here make sure we can smaller set
                 // numbers at the end of the acceptance code, i.e., at
                 // the front of the output.
-                auto a = fin(s) & std::move(c);
-                std::swap(a, c);
+                f = fin(s) & f;
               }
             else                // Positive variable? -> Inf
               {
@@ -937,10 +936,8 @@ namespace spot
                 cube = h;
               }
           }
-        c &= inf(i);
         // See comment above for the order.
-        c |= std::move(rescode);
-        std::swap(c, rescode);
+        rescode = (inf(i) & f) | rescode;
       }
 
     return rescode;
@@ -986,7 +983,7 @@ namespace spot
     while ((cube = isop.next()) != bddfalse)
       {
         mark_t m = 0U;
-        acc_code c = f();
+        acc_code i = f();
         while (cube != bddtrue)
           {
             // The acceptance set associated to this BDD variable
@@ -1000,8 +997,7 @@ namespace spot
                 // The strange order here make sure we can smaller set
                 // numbers at the end of the acceptance code, i.e., at
                 // the front of the output.
-                auto a = inf(s) | std::move(c);
-                std::swap(a, c);
+                i = inf(s) | i;
               }
             else                // Positive variable? -> Fin
               {
@@ -1009,10 +1005,8 @@ namespace spot
                 cube = h;
               }
           }
-        c |= fin(m);
         // See comment above for the order.
-        c &= std::move(rescode);
-        std::swap(c, rescode);
+        rescode = (fin(m) | i) & rescode;
       }
     return rescode;
   }

spot/twaalgos/cleanacc.cc
@@ -193,8 +193,7 @@ namespace spot
               for (auto m: seen_fin.sets())
                 inf.front().mark -= complement[m];
 
-              res &= inf;
-              return res;
+              return inf & res;
             }
           case acc_cond::acc_op::Or:
             {
@@ -225,8 +224,7 @@ namespace spot
               for (auto m: seen_inf.sets())
                 fin.front().mark -= complement[m];
 
-              res |= fin;
-              return res;
+              return res | fin;
             }
           case acc_cond::acc_op::Fin:
             return acc_cond::acc_code::fin(pos[-1].mark);

tests/core/acc.test
@@ -64,12 +64,12 @@ stripping
 #1: {2}
 2 Inf(0)&Inf(1)&Inf(3) 1
 5 Fin(2) | (Inf(0)&Inf(1)&Inf(3)) 1
-7 Fin(0) | Fin(2) | (Inf(0)&Inf(1)&Inf(3)) 1
-7 Fin(0) | Fin(2) | (Inf(0)&Inf(1)&Inf(3)) 1
-7 Fin(0) | Fin(2) | (Inf(0)&Inf(1)&Inf(3)) 1
-10 (Fin(0)|Fin(1)) & (Fin(0) | Fin(2) | (Inf(0)&Inf(1)&Inf(3))) 0
+5 (Fin(0)|Fin(2)) | (Inf(0)&Inf(1)&Inf(3)) 1
+5 (Fin(0)|Fin(2)) | (Inf(0)&Inf(1)&Inf(3)) 1
+5 (Fin(0)|Fin(2)) | (Inf(0)&Inf(1)&Inf(3)) 1
+8 (Fin(0)|Fin(1)) & ((Fin(0)|Fin(2)) | (Inf(0)&Inf(1)&Inf(3))) 0
 2 f 1
-9 (Fin(0)|Fin(1)) | Fin(0) | Fin(2) | (Inf(0)&Inf(1)&Inf(3)) 1
+5 (Fin(0)|Fin(1)|Fin(2)) | (Inf(0)&Inf(1)&Inf(3)) 1
 5 (Fin(2)|Fin(3)) & (Inf(0)&Inf(1)) 0
 (Fin(2)|Fin(3)) & (Inf(0)&Inf(1)) {0} true
 {1}
@@ -82,9 +82,9 @@ f
 Fin(2)
 Inf(2)
 Fin(2) | Inf(2)
-Fin(2) & Inf(2)
-Fin(0) | (Fin(2) & Inf(1)) | Fin(3)
-Fin(0) | (Fin(2) & Inf(1)) | Fin(3)
+Inf(2) & Fin(2)
+(Fin(0)|Fin(3)) | (Inf(1) & Fin(2))
+(Fin(0)|Fin(3)) | (Inf(1) & Fin(2))
 EOF
 
 run 0 ../acc | tee stdout

tests/core/acc2.test
@@ -1,6 +1,6 @@
 #!/bin/sh
 # -*- coding: utf-8 -*-
-# Copyright (C) 2015 Laboratoire de Recherche et Développement
+# Copyright (C) 2015, 2017 Laboratoire de Recherche et Développement
 # de l'Epita (LRDE).
 #
 # This file is part of Spot, a model checking library.
@@ -71,13 +71,13 @@ diff acceptances output
 
 #------------- CNF -------------
 
-res="(Fin(2) | Inf(1)) & (Fin(1) | Inf(3)) & Inf(0)"
+res="Inf(0) & (Inf(1) | Fin(2)) & (Inf(3) | Fin(1))"
 cat >acceptances<<EOF
 2 Inf(0)&Inf(1), 2 Inf(0)&Inf(1)
 2 Fin(0) & Inf(1), 2 Fin(0) & Inf(1)
 2 t, 2 t
 2 f, 2 f
-3 (Inf(1) | Fin(2)) & Inf(0), 3 (Fin(2) | Inf(1)) & Inf(0)
+3 (Inf(1) | Fin(2)) & Inf(0), 3 Inf(0) & (Inf(1) | Fin(2))
 4 (Fin(1) & Fin(2) & Inf(0)) | (Inf(0)&Inf(1)&Inf(3)), 4 $res
 4 $res, 4 $res
 3 (Fin(0) & Fin(2)) | (Fin(1) & Fin(2)), 3 (Fin(0)|Fin(1)) & Fin(2)
@@ -94,7 +94,7 @@ diff acceptances output
 #------------- COMP -------------
 
 a="(Inf(1) | Fin(2)) & (Fin(1) | Inf(3)) & Inf(0)"
-b="(Fin(1) & Inf(2)) | (Fin(3) & Inf(1)) | Fin(0)"
+b="(Fin(1) & Inf(2)) | (Inf(1) & Fin(3)) | Fin(0)"
 cat >acceptances<<EOF
 2 Inf(0)&Inf(1), 2 Fin(0)|Fin(1)
 2 Fin(0) & Inf(1), 2 Inf(0) | Fin(1)
@@ -102,7 +102,7 @@ cat >acceptances<<EOF
 2 f, 2 t
 3 (Inf(1) | Fin(2)) & Inf(0), 3 (Fin(1) & Inf(2)) | Fin(0)
 4 $a, 4 $b
-4 $b, 4 (Inf(1) | Fin(2)) & (Inf(3) | Fin(1)) & Inf(0)
+4 $b, 4 (Inf(1) | Fin(2)) & (Fin(1) | Inf(3)) & Inf(0)
 3 (Fin(0)|Fin(1)) & Fin(2), 3 (Inf(0)&Inf(1)) | Inf(2)
 EOF
 

tests/core/parseaut.test
@@ -2508,7 +2508,7 @@ HOA: v1
 States: 1
 Start: 0
 AP: 2 "a" "b"
-Acceptance: 4 (Fin(1) & Inf(0)) | (Fin(3) & Inf(2))
+Acceptance: 4 (Inf(0) & Fin(1)) | (Inf(2) & Fin(3))
 properties: trans-labels explicit-labels trans-acc complete
 properties: deterministic
 --BODY--
@@ -2522,7 +2522,7 @@ HOA: v1
 States: 1
 Start: 0
 AP: 2 "a" "b"
-Acceptance: 4 (Fin(3) & Inf(2)) | (Fin(1) & Inf(0))
+Acceptance: 4 (Fin(3) & Inf(2)) | (Inf(0) & Fin(1))
 properties: trans-labels explicit-labels trans-acc complete
 properties: deterministic
 --BODY--
@@ -2536,7 +2536,7 @@ HOA: v1
 States: 1
 Start: 0
 AP: 2 "a" "b"
-Acceptance: 6 (Inf(1) | (Fin(2)|Fin(3))) & (Inf(4) | Fin(5)) & Inf(0)
+Acceptance: 6 Inf(0) & (Inf(1) | (Fin(2)|Fin(3))) & (Inf(4) | Fin(5))
 properties: trans-labels explicit-labels trans-acc complete
 properties: deterministic
 --BODY--
@@ -2550,7 +2550,7 @@ HOA: v1
 States: 1
 Start: 0
 AP: 2 "a" "b"
-Acceptance: 6 (Inf(4) | Fin(5)) & (Inf(1) | (Fin(2)|Fin(3))) & Inf(0)
+Acceptance: 6 (Fin(5) | Inf(4)) & Inf(0) & ((Fin(2)|Fin(3)) | Inf(1))
 properties: trans-labels explicit-labels trans-acc complete
 properties: deterministic
 --BODY--
@@ -2624,7 +2624,7 @@ HOA: v1
 States: 1
 Start: 0
 AP: 2 "a" "b"
-Acceptance: 4 Fin(0) & ((Fin(2) & Inf(3)) | Inf(1))
+Acceptance: 4 Fin(0) & ((Inf(3) & Fin(2)) | Inf(1))
 properties: trans-labels explicit-labels trans-acc complete
 properties: deterministic
 --BODY--

tests/core/readsave.test
@@ -470,7 +470,7 @@ EOF
 cat >expected <<EOF
 digraph G {
   rankdir=LR
-  label="Fin(⓿) | (Fin(❶) & Inf(❷)) | Fin(❸)"
+  label="(Fin(⓿)|Fin(❸)) | (Fin(❶) & Inf(❷))"
   labelloc="t"
   I [label="", style=invis, width=0]
   I -> 0
@@ -524,7 +524,7 @@ diff out expected
 cat >expected2 <<EOF
 digraph G {
   rankdir=LR
-  label="Fin(⓿) | (Fin(❶) & Inf(❷)) | Fin(❸)"
+  label="(Fin(⓿)|Fin(❸)) | (Fin(❶) & Inf(❷))"
   labelloc="t"
   node [shape="circle"]
   I [label="", style=invis, width=0]

commit f8ef06acc603b144471746d98a9db5f24bd89fec

acc: fix operator| and operator&
Bug introduced in abe2c08b, visible in tests/python/product.ipynb,
and tests/python/acc_cond.ipynb.

* spot/twa/acc.hh: fix operator| and operator&.
* tests/python/acc_cond.ipynb: Adjust test case.

spot/twa/acc.hh
@@ -686,7 +686,9 @@ namespace spot
 
       acc_code operator&(const acc_code& r)
       {
-        return *this &= r;
+        acc_code res = *this;
+        res &= r;
+        return res;
       }
 
       acc_code operator&(acc_code&& r)
@@ -789,9 +791,11 @@ namespace spot
         return res;
       }
 
-      acc_code& operator|(const acc_code& r)
+      acc_code operator|(const acc_code& r)
       {
-        return *this |= r;
+        acc_code res = *this;
+        res |= r;
+        return res;
       }
 
       acc_code& operator<<=(unsigned sets)

tests/python/acc_cond.ipynb
@@ -15,7 +15,7 @@
    "name": "python",
    "nbconvert_exporter": "python",
    "pygments_lexer": "ipython3",
-   "version": "3.5.3+"
+   "version": "3.5.4"
   },
   "name": ""
  },
@@ -720,8 +720,8 @@
        "output_type": "stream",
        "stream": "stdout",
        "text": [
-        "((Fin(4) & Inf(5)) | (Fin(6) & Inf(7))) & ((Fin(0) & Inf(1)) | (Fin(2) & Inf(3)))\n",
-        "((Inf(4) | Fin(5)) & (Inf(6) | Fin(7))) | ((Inf(0) | Fin(1)) & (Inf(2) | Fin(3)))\n"
+        "(Fin(0) & Inf(1)) | (Fin(2) & Inf(3))\n",
+        "(Inf(0) | Fin(1)) & (Inf(2) | Fin(3))\n"
        ]
       }
      ],
@@ -1426,15 +1426,6 @@
       }
      ],
      "prompt_number": 52
-    },
-    {
-     "cell_type": "code",
-     "collapsed": false,
-     "input": [],
-     "language": "python",
-     "metadata": {},
-     "outputs": [],
-     "prompt_number": null
     }
    ],
    "metadata": {}