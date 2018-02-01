5dbe3c073743dba3e9c1a13fba36225d03560d67
Alexandre Duret-Lutz <adl@lrde.epita.fr>
2018-02-01 21:53:00 +0100

    tests: cleanup temporary files leftover by a test case
    * tests/python/otfcrash.py: Remove the file generated by divine.

--- tests/python/otfcrash.py
+++ tests/python/otfcrash.py
@@ -1,5 +1,25 @@
+# -*- mode: python; coding: utf-8 -*-
+# Copyright (C) 2016, 2018  Laboratoire de Recherche et Développement
+# de l'Epita
+#
+# This file is part of Spot, a model checking library.
+#
+# Spot is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# Spot is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+# License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import spot
 import spot.ltsmin
+import spot.aux
 import tempfile
 import shutil
 import sys
@@ -22,6 +42,8 @@ system async;
 """)
     fp.flush()
     m = spot.ltsmin.load(fp.name)
+    spot.aux.rm_f(fp.name + '.cpp')
+    spot.aux.rm_f(fp.name + '2C')
 
     def modelcheck(formula, model):
         a = spot.translate(formula)
